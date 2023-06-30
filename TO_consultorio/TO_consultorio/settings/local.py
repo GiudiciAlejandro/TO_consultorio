5ee74978e91e2fe644ba8c0e68f399d8062fb169
Alejandro <giudicialejandro@gmail.com>
2023-06-30 20:15:50 -0300

    db created

--- TO_consultorio/TO_consultorio/settings/local.py
+++ TO_consultorio/TO_consultorio/settings/local.py
@@ -11,10 +11,15 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'consultorio',
+        'USER': 'postgres',
+        'PASSWORD': 'Alejandro',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Static files (CSS, JavaScript, Images)
