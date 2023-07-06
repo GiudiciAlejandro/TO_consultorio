e71d6fd8219d215eb9af46a1dad03425db36abf9
Alejandro <giudicialejandro@gmail.com>
2023-07-05 22:18:37 -0300

    sigue el intento de una basehtml

--- TO_consultorio/templates/base/base.js
+++ TO_consultorio/templates/base/base.js
@@ -1,11 +1,1 @@
 const menu = document.querySelector(".menu");
-const open_menu = document.querySelector(".open-menu");
-const close_menu = document.querySelector(".close-menu");
-
-function toggleMenu() {
-    menu.classList.toggle("menu_opened");
-
-}
-
-open_menu.addEventListener("click", toggleMenu);
-close_menu.addEventListener("click", toggleMenu);
