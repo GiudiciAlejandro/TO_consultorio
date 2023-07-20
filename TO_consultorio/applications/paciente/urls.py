9d54f04fad205929f5b173d27e3d5f8b8cf7c2cb
alejandro <giudicialejandro@gmail.com>
2023-07-20 20:38:54 -0300

    paciente DetailView

--- TO_consultorio/applications/paciente/urls.py
+++ TO_consultorio/applications/paciente/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path, include
 from . import views
-from .views import Lista_paciente, nuevo_paciente
+from .views import PacienteListview, Nuevo_paciente, PacienteDetailView
 
 urlpatterns = [
-    path('lista_paciente/', Lista_paciente.as_view(), name='lista_paciente'),
-    path('nuevo_paciente/', nuevo_paciente.as_view(), name='nuevo_paciente'),
-
+    path('lista_paciente/', PacienteListview.as_view(), name='lista_paciente'),
+    path('nuevo_paciente/', Nuevo_paciente.as_view(), name='nuevo_paciente'),
+    path('detalle_paciente/<int:pk>/', PacienteDetailView.as_view(), name='detalle_paciente'),
 ]
--- TO_consultorio/applications/paciente/views.py
+++ TO_consultorio/applications/paciente/views.py
@@ -1,20 +1,39 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.urls import reverse
-from django.views.generic.list import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 from .models import Paciente
 
 
-class Lista_paciente(ListView):
+class PacienteListview(ListView):
     model= Paciente
     template_name="paciente/lista_pacientes.html"
-    queryset = Paciente.objects.all()
+    # TODO paginate_by = 10
+    ordering = "surname"
+    context_object_name = "pacientes"
+    """ El context_object_name se carga con el get_query_set de la funcion de abajo"""
+
+    def get_queryset(self): 
+        buscado= self.request.GET.get("kword",'')
+        lista= Paciente.objects.filter(surname=buscado)
+        print(lista)
+        return lista
+
 
 
 class Nuevo_paciente(CreateView):
     model= Paciente
     template_name="paciente/nuevo_paciente.html"
     queryset = Paciente.objects.all()
+ 
+
+class PacienteDetailView(DetailView):
+    model=Paciente
+    template_name="paciente/detalle_paciente.html"
+
+
